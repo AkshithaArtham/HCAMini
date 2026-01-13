afbe9a0e4a6159c08429e5ccb3368dc1bcde1c95
AkshithaArtham <akshithaartham@gmail.com>
2026-01-13 23:59:32 +0530

    Patient creation

--- obj/Debug/net10.0/HCAMiniEHR.AssemblyInfo.cs
+++ obj/Debug/net10.0/HCAMiniEHR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HCAMiniEHR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed4e27a32f5cccd1dacd63bcd1c0f1646e71592a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HCAMiniEHR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HCAMiniEHR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
